1e9232c1ce2eb1418abf9678f5f09732f9bea7c6
reesaldle-dev <jamesziegler@proton.me>
2024-12-30 15:50:48 -0600

    uh so i fixed it

---
+++ public/scripts/random-texts.js
@@ -0,0 +1,16 @@
+const splashTexts = [
+    "Welcome to Velara!",
+    "ahahahhah",
+    "axon sucks, fight me!",
+    "you didnt even see this",
+    "what did you just do",
+    "i like the sound of this",
+    "i dont think this is a good idea",
+    "watermelon > chicken",
+    "Velara > 55gms",
+    "i dont know what this is"
+];
+
+const splashTextElement = document.getElementById('random-texts');
+const randomIndex = Math.floor(Math.random() * splashTexts.length);
+splashTextElement.textContent = splashTexts[randomIndex];
