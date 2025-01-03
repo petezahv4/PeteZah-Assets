de97dc2485fbaf3a464ceaa85949e11c77349e62
reesaldle-dev <jamesziegler@proton.me>
2025-01-02 22:31:45 -0600

    local host changed from 8080 to 3000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
