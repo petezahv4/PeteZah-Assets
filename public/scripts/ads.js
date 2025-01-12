bfc5e967d6dda7a2d5f1e6dff01bff564b19634a
reesaldle <routeus117@pm.me>
2025-01-11 20:04:15 -0600

    i fixed it .html

---
+++ public/scripts/ads.js
@@ -0,0 +1,7 @@
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1597620073084418';
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+})();
