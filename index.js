00da2ae69840b4a0a46679fadbc3fefedb57e882
reesaldle <routeus117@pm.me>
2025-02-12 18:34:39 -0600

    Update server port and search engine links; add social media icons

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 app.use(express.json());
