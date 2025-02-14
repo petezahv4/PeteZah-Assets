e07e42aab68f4fa991b78ea1493f3002473caa3a
#willo <153154914+willoo0@users.noreply.github.com>
2025-02-14 01:02:20 -0800

    Update uv.js

--- public/static/uv.js
+++ public/static/uv.js
@@ -1,6 +1,6 @@
-importScripts("/static/uv/uv.bundle.js");
-importScripts("/static/uv/uv.config.js");
-importScripts("/static/uv/uv.sw.js");
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts("/uv/uv.sw.js");
 const sw = new UVServiceWorker();
 
 self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
