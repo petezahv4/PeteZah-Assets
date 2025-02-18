1092ef1d813e351a4a06724b68eb85875b423428
reesaldle <routeus117@pm.me>
2025-02-17 18:43:00 -0600

    adsterra as punishment for using unoffical domains

---
+++ public/scripts/adsterra.js
@@ -0,0 +1,20 @@
+const blockedDomains = [
+    'gyattmath.buzz',
+    'ilovelittlekids.buzz',
+    'mathforkids.buzz',
+    'minoritymath.buzz',
+    'reesaldle.com',
+    'sunsett.cc',
+    'sunsett.icu',
+    'ubg.lat',
+    'velara.buzz',
+    'velara.cc',
+    'velaramath.buzz'
+];
+
+if (!blockedDomains.includes(location.hostname)) {
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl25889309.effectiveratecpm.com/26/25/62/262562bca6a764333732f0c2aa2d0050.js';
+    document.head.appendChild(script);
+}
